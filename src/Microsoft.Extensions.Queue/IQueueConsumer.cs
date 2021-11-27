ef7b8e390f04242b9d1eeb3b121e36c30bd9a0d9
Samuel89Su <sushoucai1989@hotmail.com>
2021-11-27 22:22:36 +0800

    add tests

--- src/Microsoft.Extensions.Queue/IQueueConsumer.cs
+++ src/Microsoft.Extensions.Queue/IQueueConsumer.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Threading.Channels.Queue
 {
-    public interface IQueueConsumer<T> : IQueueConsumer<T, IQueue<T>>
+    public interface IQueueConsumer<T> : IQueueConsumer<T, IQueue<T>> where T : IEventData
+    { }
+
+    public interface IQueueConsumer<T, TQueue, TEventHandler> : IQueueConsumer<T, TQueue> where TQueue : IQueue<T> where TEventHandler : IEventHandler<T> where T : IEventData
     { }
 
     public interface IQueueConsumer<T, TQueue> where TQueue : IQueue<T>
     {
-        IAsyncEnumerable<T> ReadAllAsync([EnumeratorCancellation] CancellationToken cancellationToken = default);
+        Task StartConsume();
     }
 }
