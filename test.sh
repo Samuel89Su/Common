a0b5b3c29a6948488a57fc0e0c9bb3e4c81d20e5
Samuel89Su <sushoucai1989@hotmail.com>
2021-11-28 12:21:05 +0800

    update test script

--- test.sh
+++ test.sh
@@ -1,8 +1,17 @@
+## remove last testresult
 rm -r ./test/TestResults/*
 
+## run test
 dotnet test -c:Release -r:"test/TestResults" --collect:"XPlat Code Coverage"
 
+## generate report
 for report in $(find ./test/TestResults -name "coverage.cobertura.xml");
 do
   dotnet ./test/reportgenerator/4.8.0/tools/net5.0/ReportGenerator.dll "-reports:$report" "-targetdir:test/coveragereport" -reporttypes:Html "-historydir:test/coveragereport/history"
 done
+
+## add report to git
+git add test/coveragereport/
+
+## commit report
+git commit -m 'update coverage report'
